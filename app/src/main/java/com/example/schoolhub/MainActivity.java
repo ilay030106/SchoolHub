8c0d2fd02be9037113fbdc04988a59475dc491fd
ilay030106 <ilay8073@gmail.com>
2025-01-23 21:48:06 +0200

    added animations for swapping fragments

--- app/src/main/java/com/example/schoolhub/MainActivity.java
+++ app/src/main/java/com/example/schoolhub/MainActivity.java
@@ -24,11 +24,25 @@ import com.google.firebase.firestore.FirebaseFirestore;
 public class MainActivity extends AppCompatActivity {
     BottomNavigationView menu;
 
+    private int currentMenuItemId = R.id.navigation_home; // Default to home menu
 
-    public void replaceFragment(Fragment fragment) {
+    public void replaceFragment(Fragment fragment, boolean isMovingRight) {
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        fragmentTransaction.setCustomAnimations(android.R.anim.slide_in_left, android.R.anim.slide_out_right);
+
+        // Set animations based on direction
+        if (isMovingRight) {
+            fragmentTransaction.setCustomAnimations(
+                    R.anim.slide_in_left,  // Enter animation
+                    R.anim.slide_out_right // Exit animation
+            );
+        } else {
+            fragmentTransaction.setCustomAnimations(
+                    R.anim.slide_in_right,  // Enter animation
+                    R.anim.slide_out_left  // Exit animation
+            );
+        }
+
         fragmentTransaction.replace(R.id.AppFragmentContainer, fragment);
         fragmentTransaction.commit();
     }
@@ -38,19 +52,24 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
         menu = findViewById(R.id.menu);
 
         menu.setOnItemSelectedListener(item -> {
             Fragment fragment = null;
+            boolean isMovingRight = false;
+
             int itemId = item.getItemId();
             if (itemId == R.id.navigation_home) {
                 fragment = new homeFragment();
+                isMovingRight = itemId > currentMenuItemId; // Determine direction
             } else if (itemId == R.id.navigation_timetable) {
                 fragment = new TimeTableFragment();
+                isMovingRight = itemId > currentMenuItemId; // Determine direction
             }
+
             if (fragment != null) {
-                replaceFragment(fragment);
+                replaceFragment(fragment, isMovingRight);
+                currentMenuItemId = itemId; // Update current item ID
                 return true;
             }
             return false;
